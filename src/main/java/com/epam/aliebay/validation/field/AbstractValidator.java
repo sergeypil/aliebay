5b996335de4d81a4fd052379c2ba1e29a9294a9e
Northug <47028633+Northug@users.noreply.github.com>
2022-01-16 00:20:07 +0600

    update validator

--- src/main/java/com/epam/aliebay/validation/field/AbstractValidator.java
+++ src/main/java/com/epam/aliebay/validation/field/AbstractValidator.java
@@ -1,5 +1,6 @@
 package com.epam.aliebay.validation.field;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class AbstractValidator {
@@ -11,5 +12,20 @@ public class AbstractValidator {
         this.value = value;
         this.attributeOfError = attributeOfError;
         this.attributes = attributes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractValidator)) return false;
+        AbstractValidator that = (AbstractValidator) o;
+        return Objects.equals(value, that.value) &&
+                Objects.equals(attributeOfError, that.attributeOfError) &&
+                Objects.equals(attributes, that.attributes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, attributeOfError, attributes);
     }
 }
